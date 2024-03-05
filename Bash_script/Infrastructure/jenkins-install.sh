8e4bc4cb6cb86faba4eae31a531b7f8824d3a01a
Long Truong <longth2162000@gmail.com>
2024-03-05 14:40:20 +0700

    Update script to install Jenkins Server
    Signed-off-by: Long Truong <longth2162000@gmail.com>

--- Bash_script/Infrastructure/jenkins-install.sh
+++ Bash_script/Infrastructure/jenkins-install.sh
@@ -9,8 +9,10 @@ java -version
 # tải xuống tệp khóa gốc của Jenkins từ URL và sau đó thêm khóa này vào hệ thống để xác thực các gói phần mềm Jenkins trước khi cài đặt chúng
 wget -p -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
 apt-get update
 apt install jenkins -y
 systemctl start jenkins
-ufw allow 8080
-ufw enable
+systemctl enable jenkins
+#ufw allow 8080
+#ufw enable
